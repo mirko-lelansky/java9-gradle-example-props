90423e082f42c465c2f3569ba877df00b428305d
mlelansky <mlelansky@mail.de>
2017-11-01 10:36:32 +0100

    Refactoring the class

--- src/main/java/org/example/props/Launcher.java
+++ src/main/java/org/example/props/Launcher.java
@@ -7,18 +7,39 @@ import java.util.Properties;
 public final class Launcher {
 
     public static void main(String[] args) {
+        final String propertiesFile = "config.properties";
+        final Module mod = Launcher.class.getModule();
+        InputStream modResource = null;
+        InputStream classResource;
+        final String applicationKey = "application.id";
+
         try {
-            final Module mod = Launcher.class.getModule();
-
-            final InputStream modResource = mod.getResourceAsStream("config.properties");
-
-            final Properties props = new Properties();
-            props.load(modResource);
-            final String applicationId = props.getProperty("application.id");
-
-            System.out.println(applicationId);
+            modResource = mod.getResourceAsStream(propertiesFile);
         } catch (IOException e) {
             System.err.println(e.getLocalizedMessage());
         }
+        classResource = Launcher.class.getResourceAsStream("../../../" + propertiesFile);
+
+        if(modResource != null || classResource != null) {
+            if(modResource != null) {
+                printProperty(modResource, applicationKey);
+            } else {
+                printProperty(classResource, applicationKey);
+            }
+        } else {
+            throw new IllegalStateException("One resource should be found.");
+        }
+    }
+
+    private static void printProperty(final InputStream propertyStream, final String key) {
+        final Properties props = new Properties();
+        try {
+            props.load(propertyStream);
+            final String value = props.getProperty(key);
+            System.out.println(value);
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+
     }
 }
